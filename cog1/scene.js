c6a9248954189b6391db5b3b16896db566be6eb5
Patrick Reinbold <reinpa@outlook.de>
2019-10-23 12:00:47 +0200

    ESA01 yay! found it!

--- cog1/scene.js
+++ cog1/scene.js
@@ -381,6 +381,7 @@ function(exports, dojo, domStyle, app, scenegraph, createScene, animation, raste
 
 		// Loop over polygons in model.
 		for(var p = 0; p < polygons.length; p++) {
+			polycount++;
 
 			// Prepare the data of polygon p to pass to scanline.
 			var polygon = polygons[p];
